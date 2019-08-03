6ad72211b7d142d8795ca25af1c986bd5fa1a7e0
Karine Agredo <karina.a182@gmail.com>
2019-08-03 10:00:41 +1000

    creation of components

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { TodoComponent } from "./todo/todo.component";
+import { ListComponent } from "./todo/list/list.component";
+import { ItemComponent } from "./todo/item/item.component";
+import { TodoFooterComponent } from "./todo/todo-footer/todo-footer.component";
+import { TodoAddComponent } from "./todo/todo-add/todo-add.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    ListComponent,
+    ItemComponent,
+    TodoFooterComponent,
+    TodoAddComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ todo-app/src/app/todo/todo-add/todo-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styles: []
+})
+export class TodoAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
